84574438b418641d34ad5e34ced1b7c9e7367cdd
Mikka Luster <mikka@mikka.is>
2021-12-20 07:26:44 +0100

    initial commit

--- main.ts
+++ main.ts
@@ -1,137 +1,66 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Plugin } from 'obsidian';
+import { stringify } from 'yaml';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+function arrayUnique(array) {
+    var a = array.concat();
+    for(var i=0; i<a.length; ++i) {
+        for(var j=i+1; j<a.length; ++j) {
+            if(a[i] === a[j])
+                a.splice(j--, 1);
+        }
+    }
+    return a;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+function parsebody (body) {
+	var itt = []
+	let string
+	const regexp = /(^|\s|\[|\()#([a-zA-Z_-\d-]+)/g
+	var tags = body.matchAll(regexp)
+	for (const match of tags) {
+		string = match[0].toLocaleLowerCase().trim().substring(1)
+		if (string.charAt(0) === "#") { string = string.substring(1) }
+		itt.push(string)
+	}
+	return itt
 }
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class FmTags extends Plugin {
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: 'fmtags_edit',
+			name: 'Move tags to Frontmatter',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+				
+				const file = app.workspace.getActiveFile();
+				let text = editor.getDoc().getValue();
+				var frontMatter = JSON.parse(JSON.stringify(app.metadataCache.getFileCache(file)?.frontmatter));
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				let end = frontMatter.position.end.line + 1;
+				let content = text.split("\n").slice(end).join("\n");
+				delete frontMatter["position"]
+
+				const tags_in_body = parsebody(content)
+				console.log(tags_in_body)
+				
+				if (frontMatter["Tags"]) {
+					var tags_in_matter = frontMatter["Tags"].split(",")
+					tags_in_matter = tags_in_matter.map(tagg => tagg.toLocaleLowerCase().trim())
+				} else {
+					var tags_in_matter = []
 				}
+				
+				var all_tags=arrayUnique(tags_in_matter.concat(tags_in_body));
+				if (all_tags.length >= 1) {
+					frontMatter["Tags"] = all_tags.join(", ")
+				} else {
+					delete frontMatter["Tags"]
+				}
+				const final_content = `---\n${stringify(frontMatter)}---\n${content}`;
+				editor.setValue(final_content)
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
-
 	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
